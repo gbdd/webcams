abfb4f0be691782fe0cef7d4bcaf1f7f76dc94f5
Gerald <gboudaud73@gmail.com>
2023-03-22 12:20:46 +0100

    Déplacement de la carte pour la remettre à sa position d'avant ouverture du popup

--- src/components/WebcamMap.tsx
+++ src/components/WebcamMap.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useEffect } from 'react';
+import React, { FC, ReactElement, useEffect, useState } from 'react';
 import './WebcamMap.css';
 import { TileLayer, useMap, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
@@ -27,6 +27,9 @@ export const WebcamMap: FC<WebcamMapProps> = ({webcams}): ReactElement => {
     }
   , [map]);
 
+  const [popupOpened, setPopupOpened] = useState<boolean>(false);
+  const [centerBeforePopup, setCenterBeforePopup] = useState<(L.LatLng | null)>(null);
+
   useMapEvents({
     zoomend: (evt) => {
       if (evt?.target?._zoom) {
@@ -34,10 +37,23 @@ export const WebcamMap: FC<WebcamMapProps> = ({webcams}): ReactElement => {
       }
     },
     moveend: (evt) => {
-      if (evt?.target?._lastCenter) {
+      if (!popupOpened && evt?.target?._lastCenter) {
         setPreference(PREFERENCES.MAP_LAST_CENTER, evt.target._lastCenter);
       }
-    }
+    },
+    popupopen: (evt) => {
+      setPopupOpened(true);
+      setCenterBeforePopup(map.getCenter());
+    },
+    popupclose: (evt) => {
+      if (popupOpened) {
+        setPopupOpened(false);
+        if (centerBeforePopup !== null) {
+          map.setView(centerBeforePopup);
+        }
+      }
+      setCenterBeforePopup(null);
+    },
   })
 
   // TOPO LAYER  url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
